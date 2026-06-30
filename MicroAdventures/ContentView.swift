7d3193e887e00a23d61383e26260554016d2000e
Tal Teri <talteri95@gmail.com>
2026-06-30 22:29:16 +0300

    Initial Commit

---
+++ MicroAdventures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MicroAdventures
+//
+//  Created by Tal Teri on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicroAdventures/MicroAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventuresApp.swift
+//  MicroAdventures
+//
+//  Created by Tal Teri on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MicroAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
